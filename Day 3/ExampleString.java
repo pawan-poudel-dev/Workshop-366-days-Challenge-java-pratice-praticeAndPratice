42d3cdd145b8e63966eb7a0f12cc50d39ffa6bfb
pawan_poudel_Devloper <poudelpawan770@gmail.com>
2026-04-10 22:05:15 +0545

    Add files via upload

---
+++ Day 3/ExampleString.java
@@ -0,0 +1,24 @@
+public class ExampleString {
+ public static void main(String [] args){
+       /*  String nam1 = new String("Programmiz");
+        String nam2 = new String("Programmiz");
+        System.out.println("Check if two string are equal or not.");
+        // using == operator and seet the result 
+        boolean result1 = (nam1 == nam2);
+        System.out.println("Uising == operator "+ result1);
+        // uisnng the .equals(),
+        boolean result2 = nam1.equals(nam2);
+        System.out.println("Using eauals:"+ result2);
+ } */
+ String city = "Pokhara";
+  city = city.toUpperCase();
+ System.out.println(city);
+ String a = "Hello";
+ String b = "Hello";
+ String c = new String("Hello");
+ System.out.println(a==b);
+ System.out.println(a ==c);
+ 
+ System.out.println(a.equals(b));
+ }
+}
