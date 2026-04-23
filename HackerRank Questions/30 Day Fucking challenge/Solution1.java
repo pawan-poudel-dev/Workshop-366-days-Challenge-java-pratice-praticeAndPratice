8efae75fc1caa77ac6ee64a13e3f42e22e43f978
pawan_poudel_Devloper <poudelpawan770@gmail.com>
2026-04-23 19:29:16 +0545

    Add files via upload

---
+++ HackerRank Questions/30 Day Fucking challenge/Solution1.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution1 {
+    static int B;
+    static int H;
+    static boolean flag = true;
+
+    static {
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt(); // Breadth of parallelogram
+        H = sc.nextInt(); // Height of parallelogram
+
+        if (B <= 0 || H <= 0) {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = B * H;
+            System.out.println(area);
+        }
+    }
+}
